928124de2306369362691569c03ad76197600c60
winishasmith <winishasmith@gmail.com>
2016-06-20 02:16:00 -0400

    required csv in application.rb file

--- csv-uploadr/app/controllers/defects_controller.rb
+++ csv-uploadr/app/controllers/defects_controller.rb
@@ -1,9 +1,11 @@
 class DefectsController < ApplicationController
 
+  # GET /defects
   def index
     @defects = Defect.all
   end
 
+  # Upload method for CSV files
   def upload
    Defect.upload(params[:file])
    redirect_to defects_path, notice: "Upload Successful"
--- csv-uploadr/config/application.rb
+++ csv-uploadr/config/application.rb
@@ -1,5 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
+#enables the use of CSV library
+require 'csv'
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
