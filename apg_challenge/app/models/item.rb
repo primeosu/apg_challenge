612e5b1b6bd10cf7bf7a757ecb0d187edea4a8ca
Vernon Govan <vernon.govan@gmail.com>
2016-08-11 00:57:03 -0500

    Add ability to import a file

--- apg_challenge/app/models/item.rb
+++ apg_challenge/app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  require 'csv'
+
+  def self.import(file)
+    CSV.foreach(file.path, :headers => true) do |row|
+      items_hash = row.to_hash
+      Item.create!(
+          md5: items_hash['MD5'],
+          classificationName: items_hash['ClassificationName'],
+          classificationType: items_hash['ClassificationType'],
+          size: items_hash['Size'],
+          fileType: items_hash['FileType'])
+    end
+  end
 end
