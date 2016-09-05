b3dfd128bd60da7547e660dc7eff24a4759567b5
pplum09 <ir3@umbc.edu>
2016-09-05 16:44:41 -0400

    initial deploy test

---
+++ public/upload-csv.js
@@ -0,0 +1,35 @@
+$("#file-upload-btn").click(function() {
+    
+    var files = $("#file-upload-input").get(0).files;
+    
+    if (files.length <= 0) {
+        console.log("error");
+    }
+    
+    else {
+        
+        // create ajax request
+       var formData = new FormData();
+        
+        for (var i = 0; i < files.length; i++) {
+            var file = files[i];
+            formData.append("uploads[]", file, file.name);
+        }
+        
+        // execute request
+        $.ajax({
+            
+            url: "/load",
+            type: "POST",
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(data) {
+                console.log("upload success");
+            },
+            error: function(xhr, status, error) {
+                console.log("There was an error: " + error);
+            }
+        });
+    }
+});
