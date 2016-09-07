1f50294c8ec1b89d62937253ade8d706a1d5aff5
pplum09 <ir3@umbc.edu>
2016-09-07 02:27:22 -0400

    should sleep soon

--- public/upload-csv.js
+++ public/upload-csv.js
@@ -16,7 +16,7 @@ $("#file-upload-btn").click(function() {
         }
         
         // execute request
-        
+        /*
         execute_request("/load", "POST", formData, false, function(result) {
             console.log("SUCCESS!!!!");
             execute_request("/draw", "GET", null, "json", function(result) {
@@ -27,6 +27,41 @@ $("#file-upload-btn").click(function() {
                 
             });
         });
+        */
+        $.ajax({ 
+        url: "/load",
+        type: "POST",
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function(result) {
+            console.log("success 1 res: " + result); 
+            console.log("success 1 len: " + result.length); 
+            
+            $.ajax({ 
+        url: "/draw",
+        type: "GET",
+        processData: true,
+        contentType: "JSON",
+        success: function(results) {
+            console.log("success 2 res: " + results); 
+            console.log("success 2 len: " + results.length); 
+        },
+        error: function(xhr, status, error) {
+            console.log("AJAX: fail");
+            console.log("There was an error: " + error);
+            console.log("Status: " + status);
+        }
+    });
+            
+        },
+        error: function(xhr, status, error) {
+            console.log("AJAX: fail");
+            console.log("There was an error: " + error);
+            console.log("Status: " + status);
+        }
+    });
+        
     }
 });
 
