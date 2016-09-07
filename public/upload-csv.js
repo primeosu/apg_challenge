15e730d14ab078d4b02a62edf4f73f5a940b9ddc
pplum09 <ir3@umbc.edu>
2016-09-06 23:59:18 -0400

    wrk

--- public/upload-csv.js
+++ public/upload-csv.js
@@ -3,7 +3,7 @@ $("#file-upload-btn").click(function() {
     var files = $("#file-upload-input").get(0).files;
     
     if (files.length <= 0) {
-        console.log("error");
+        console.log("no files chosen");
     }
     
     else {
@@ -22,15 +22,15 @@ $("#file-upload-btn").click(function() {
 });
 
 function execute_request(url, operation, data) {
-    // execute request
-    $.ajax({
-            
+    
+    console.log("WHAT");
+    $.ajax({ 
         url: url,
         type: operation,
         data: data,
         processData: false,
         contentType: false,
-        success: function(result {
+        success: function(result) {
             console.log("AJAX: success");
             console.log("data: " + result);
         },
