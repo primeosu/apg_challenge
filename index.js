9cb7710a60f1a1297908537d8c7a7f17935f60fb
pplum09 <ir3@umbc.edu>
2016-09-07 00:18:54 -0400

    ugh

--- index.js
+++ index.js
@@ -40,40 +40,15 @@ app.post("/load", function(req, res) {
             
             var lines = data.trim().split("\n");
             var input = parse(lines);
-            var sendCode = function(res) {
-                console.log("server sending response");
-                res.sendStatus(200);
-            }
-            
-            // bottleneck?
-            pg.connect(process.env.DATABASE_URL, function(err, client, done, sendCode, res) {
-                
-                // perform all queries in array
-                for (var i = 0; i < input.length; i++) {
-                    
-                   
-                    // check for sql injections later
-                    client.query('insert into malware_table values ($1, $2, $3, $4, $5)', [input[i].md5, input[i].classification_name, input[i].classification_type, input[i].size, input[i].file_type], function(err, result) {
-                        done();
-                        if (err) {
-                            error = err;
-                            console.error("Error while post query: " + err); 
-                        }
-
-                        else { 
-                            console.log("Successful Query!");
-                        }
-                    });
-                  
-                }
-                sendCode(res);
-            });
-            
            
-          
             
+            // perform all queries in array
+            for (var i = 0; i < input.length; i++) {
+                executeQuery(input[i]);   
+            }
         });
     });
+});
     
     // redo
     form.on("error", function(err) {
@@ -113,4 +88,23 @@ function parse (lines) {
     }
     
     return data;
+}
+
+function executeQuery(input) {
+    
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+                
+        // check for sql injections later
+        client.query('insert into malware_table values ($1, $2, $3, $4, $5)', [input.md5, input.classification_name, input.classification_type, input.size, input.file_type], function(err, result) {
+            done();
+            if (err) {
+                error = err;
+                console.error("Error while post query: " + err); 
+            }
+
+            else { 
+                console.log("Successful Query!");
+            }
+        });              
+    });
 }
