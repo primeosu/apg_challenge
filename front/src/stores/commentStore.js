1d85026ad0f328da75ae57b4dd43bd4f7319a3e6
FrederickIge <duelarm@gmail.com>
2019-06-09 16:02:56 -0400

    removed logic from mobx stores

--- front/src/stores/commentStore.js
+++ front/src/stores/commentStore.js
@@ -2,41 +2,7 @@ import {observable, computed} from 'mobx';
 import Comment from '../models/Comment';
 
 class CommentStore {
-  constructor(root) {
-    this.root = root;
-  }
-
-  @observable comments = [];
-
-  fireStoreData;
-
-  getCommentsByGooglePlaceId = async () => {
-    this.comments = [];
-
-    this.fireStoreData = await this.root.fireStore.getCommentsByGooglePlaceId(this.root.spotStore.selectedSpot.googlePlaceId);
-console.log(this.fireStoreData)
-    return this.fireStoreData.empty ? null : this.displayComments();
-  };
-
-  displayComments() {
-    this.fireStoreData.forEach(doc => {
-      let comment = new Comment(doc);
-      this.comments.push(comment);
-    });
-  }
-
-  deleteComment = async (id) =>{
-    await this.root.fireStore.deleteComment(id);
-
-    const result = this.comments.filter(comment => comment.id != id);
-
-    this.comments = result;
-  }
-
-  @computed get hasCommented() {
-    let result = this.comments.find(x => x.userId === 'pj7XQzQbVgUx2NZFtRhSK7DdY9c2');
-    return result ? true : false;
-  }
+ 
 }
 
 export default CommentStore;
